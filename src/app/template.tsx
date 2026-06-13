4a8115c18354794bba1b8b78f329788e91312f4e
Todd Perrine <toddperrine@hotmail.com>
2026-06-13 09:31:03 -0500

    Remove PR helper wrapper causing slowdown

--- src/app/template.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import type { ReactNode } from "react";
-import { StoryUxClarityEnhancer } from "./story-ux-clarity-enhancer";
-
-export default function Template({ children }: { children: ReactNode }) {
-  return <StoryUxClarityEnhancer>{children}</StoryUxClarityEnhancer>;
-}
